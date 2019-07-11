f52e518da10c09d853855f1025365e59734ed1b5
Maeve Griffin <griffinmaeve@gmail.com>
2019-07-10 20:37:23 -0700

    New edits

---
+++ app.js
@@ -0,0 +1,7 @@
+const verbIn1 = document.getElementById('verb1');
+const nounIn1 = document.getElementById('noun1');
+const adjIn1 = document.getElementById('adjective1');
+const verbOut1 = document.getElementById('inputV');
+const nounOut1 = document.getElementById('inputN');
+const adjOut1 = document.getElementById('inputA');
+
