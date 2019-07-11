415d8e75aaf7196e41c55ca4b920fa7e4fd93f97
Maeve Griffin <griffinmaeve@gmail.com>
2019-07-10 21:00:46 -0700

    All working but 3

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const verbIn1 = document.getElementById('verb1');
-const nounIn1 = document.getElementById('noun1');
-const adjIn1 = document.getElementById('adjective1');
+const verbIn1 = document.getElementById('vInput1');
+const nounIn1 = document.getElementById('nInput1');
+const adjIn1 = document.getElementById('aInput1');
 const verbOut1 = document.getElementById('inputV');
 const nounOut1 = document.getElementById('inputN');
 const adjOut1 = document.getElementById('inputA');
 
+function makeMadlib() {
+    verbOut1.textContent = verbIn1.value;
+    nounOut1.textContent = nounIn1.value;
+    adjOut1.textContent = adjIn1.vlaue;
+}
+
+
+function doThing() {
+
+}
