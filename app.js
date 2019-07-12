3658548ed688d4879ac5f4e16394a8adf9bc40ab
Maeve Griffin <griffinmaeve@gmail.com>
2019-07-11 18:04:28 -0700

    latest button still not working

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
+// Madlib inputs
 const verbIn1 = document.getElementById('vInput1');
 const nounIn1 = document.getElementById('nInput1');
 const adjIn1 = document.getElementById('aInput1');
+// Madlib fills
 const verbOut1 = document.getElementById('inputV');
 const nounOut1 = document.getElementById('inputN');
 const adjOut1 = document.getElementById('inputA');
+// Final madlib
 const finalMadlib = document.getElementById('finalResult');
 
 function makeMadlib() {
     verbOut1.textContent = verbIn1.value;
     nounOut1.textContent = nounIn1.value;
     adjOut1.textContent = adjIn1.value;
-    if(finalMadlib.class('hidden')) {
-        finalMadlib.removeClass('hidden').addClass('visible');
+    /* if(finalMadlib.className('hidden')) {
+        finalMadlib.className.remove('hidden');
     } else {
-        finalMadlib.removeClass('visible').addClass('hidden');
-    }
+        console.log('something went wrong');
+    } */
 }
 
 
